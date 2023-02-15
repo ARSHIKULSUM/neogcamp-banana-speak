6188d9f7794d740f97b6924364aaa95328e50a8c
ARSHIKULSUM <arshikulsum123@gmail.com>
2023-02-15 16:14:49 +0530

    input output processing done

---
+++ app.js
@@ -0,0 +1,11 @@
+var btn_translate = document.querySelector(".translate");
+var in_put = document.querySelector(".input");
+var out_put = document.querySelector("#output");
+//out_put.innerText ="your output will come herr"
+
+//console.log(out_put);
+
+btn_translate.addEventListener('click', function(){
+ out_put.innerText= "asgshjsaf  "  ; 
+
+})
